809b4a0d9005d9809b1acff8c6a585bd7b5933b5
Yi Zhang <zyigwu12@gmail.com>
2019-08-07 22:18:40 -0700

    f

---
+++ src/main/java/com/yi/scdf/demo/scdfdemo/ScdfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yi.scdf.demo.scdfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdfDemoApplication.class, args);
+	}
+
+}
