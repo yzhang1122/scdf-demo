196d5b30b254ec21a3072f987e0feaec15e5a921
Yi Zhang <zyigwu12@gmail.com>
2019-08-07 22:55:30 -0700

    fg

--- src/main/java/com/yi/scdf/demo/scdfdemo/ScdfDemoApplication.java
+++ src/main/java/com/yi/scdf/demo/scdfdemo/ScdfDemoApplication.java
@@ -1,13 +1,30 @@
 package com.yi.scdf.demo.scdfdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryDeployerAutoConfiguration;
+import org.springframework.cloud.deployer.spi.kubernetes.KubernetesAutoConfiguration;
+import org.springframework.cloud.deployer.spi.local.LocalDeployerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SessionAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class,
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class,
+        LocalDeployerAutoConfiguration.class,
+        CloudFoundryDeployerAutoConfiguration.class,
+        KubernetesAutoConfiguration.class
+})
+@EnableDataFlowServer
 public class ScdfDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScdfDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScdfDemoApplication.class, args);
+    }
 
 }
